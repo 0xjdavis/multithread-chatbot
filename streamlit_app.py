eac1c3908d274f884e122e2276c2f5decd40f5e1
J. Davis <98430977+0xjdavis@users.noreply.github.com>
2024-08-15 20:40:55 -0700

    Initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -127,7 +127,7 @@ else:
         """, unsafe_allow_html=True)
 
     # Calendly
-    st.sidebar.markdown("""<hr style="height:1px;border:none;color:#ccc;background-color:#ccc;" /><br />""", unsafe_allow_html=True)
+    st.sidebar.markdown('<hr style="height:1px;border:none;color:#ccc;background-color:#ccc;" /><br />', unsafe_allow_html=True)
     st.sidebar.markdown('<div style="border-radius:8px;padding:8px;background:#fff";width:100%;"><center><span style="height:12px;width:12px;background-color:#77e0b5;border-radius:50%;display:inline-block;"></span> <b>I'm available for new projects!</b><br /><a href="https://calendly.com/0xjavis" target="_blank"><button style="background:#126ff3;color:#fff;border: 1px #126ff3 solid;border-radius:8px;padding:8px">Schedule a call</button></a></center></div>', unsafe_allow_html=True)
 
     # Copyright
